d6e06fe911011e4142b93838741ff457a3aa9426
Mutter-Liam <liammutter100@gmail.com>
2024-04-02 23:18:25 -0400

    Bruh

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,20 +1,22 @@
 const imgs = document.getElementsByClassName("intro-img")
+
 for (let img of imgs){
+    let retro = true
     const switchImg = () =>{
-        switch(img.src){
-            case "RetroBenFace.webp":
-                img.src = "BenFace.jpg"
-                break;
-            case "RetroAdamFace.webp":
-                img.src = "AdamFace.jpg"
-                break;
-            case "RetroLiamFace.webp":
-                img.src = "LiamFace.jpeg"
-            break;
-            case "RetroSandroFace.webp":
-                img.src = "SandroFace.jpg"
-            break;
+        let id = img.id
+        if (id === "liam"){
+            img.src = (retro) ? "images/LiamFace.jpeg" : "images/RetroLiamFace.png"
         }
+        else if (id === "ben"){
+            img.src = (retro) ? "images/BenFace.jpg" : "images/RetroBenFace.webp"
+        }
+        else if (id === "adam"){
+            img.src = (retro) ? "images/AdamFace.jpeg" : "images/RetroAdamFace.webp"
+        }
+        else if (id === "sandro"){
+            img.src = (retro) ? "images/SandroFace.jpeg" : "images/RetroSandroFace.png"
+        }
+        retro = !retro
     }
     img.addEventListener("click",switchImg)
 }
