29bf082c156549f79541866e558d36f8289a62d9
Adam Smith <adamjsmith002@gmail.com>
2024-04-21 22:50:05 -0400

    map stuff

--- src/docs/milestone-02/src/client/script.js
+++ src/docs/milestone-02/src/client/script.js
@@ -2,17 +2,20 @@ import { renderHeaderBar } from "./componentScripts/headerBar.js";
 import { renderFeedGrid, renderBoardGrid } from "./componentScripts/gridRenderer.js";
 import { renderBoardList } from "./componentScripts/boardList.js";
 import { renderEventCreate } from "./componentScripts/eventCreate.js";
+import { renderMap } from "./componentScripts/map.js";
 
 const headerBarElement = document.getElementById("headerBar");
 const boardListElement = document.getElementById("boardList");
 const feedGridElement = document.getElementById("feedGrid");
 const boardGridElement = document.getElementById("boardGrid");
 const createEventElement = document.getElementById("eventCreate");
+const map = document.getElementById("map");
 
-let feedView = false; //Toggles board grid and feed grid
+let view = 'map';
 
 renderHeaderBar(headerBarElement);
 renderBoardList(boardListElement);
-if (feedView) renderFeedGrid(feedGridElement);
-else renderBoardGrid(boardGridElement)
+if (view === 'map') renderMap(map);
+if (view === 'feed') renderFeedGrid(feedGridElement);
+if (view === 'boards') renderBoardGrid(boardGridElement);
 renderEventCreate(createEventElement);
