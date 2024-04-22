0d15d290dc222344aba33e7f53f9e8110af1c8c1
Adam Smith <adamjsmith002@gmail.com>
2024-04-22 00:33:42 -0400

    map shenanigans + logo

--- src/docs/milestone-02/src/client/componentScripts/headerBar.js
+++ src/docs/milestone-02/src/client/componentScripts/headerBar.js
@@ -1,3 +1,6 @@
 export function renderHeaderBar(element) {
-    element.innerHTML = "<h1>Rendezvous</h1>";
+    element.innerHTML = "<div style='display: flex; align-items: center; height: 100%;'><img src='./images/RendezvousIcon.png' width='60px' height='60px'></img><h2 style='margin-left: 10px; margin-top: 0; margin-bottom: 0;'>Rendezvous</h2></div>";
 }
+
+
+
--- src/docs/milestone-02/src/client/componentScripts/map.js
+++ src/docs/milestone-02/src/client/componentScripts/map.js
@@ -1,4 +1,5 @@
 export function renderMap(element) {
+    let markerCount = 0;
     // Get a reference to the parent container
     const parentContainer = element.parentElement;
 
@@ -6,8 +7,24 @@ export function renderMap(element) {
     const parentWidth = parentContainer.offsetWidth;
     const parentHeight = parentContainer.offsetHeight;
 
-    const mapURL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17275.10389291347!2d-72.5333217714684!3d42.38777369375425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e6d273e4f0f865%3A0xa0d6586089148e05!2sUniversity%20of%20Massachusetts%20Amherst!5e0!3m2!1sen!2sus!4v1713540379345!5m2!1sen!2sus";
+    // Create a new map centered on Amherst, Massachusetts
+    const umass = { lat: 42.3868, lng: -72.5301 };
+    const map = new google.maps.Map(element, {
+        center: umass,
+        zoom: 15, // Adjust the zoom level as needed
+    });
 
-    // Set the HTML content with dynamic width and height
-    element.innerHTML = `<iframe src="${mapURL}" width=100% height=100% style="border:0;grid-column: span 3;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+    // This event listener will add a marker when the map is clicked
+    map.addListener("click", (event) => {
+        addMarker(event.latLng, map);
+    });
+
+    // Function to add a marker to the map
+    function addMarker(location, map) {
+        new google.maps.Marker({
+            position: location,
+            map: map,
+            label: "Gathering " + markerCount++
+        });
+    }
 }
