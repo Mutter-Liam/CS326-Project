29dc5f07eef2d8a3d1b1d654ba7d3e7b854d8648
ProphecyOak <drito610@gmail.com>
2024-04-24 21:06:49 -0400

    ViewChangingButtons

--- src/docs/milestone-02/src/client/componentScripts/headerBar.js
+++ src/docs/milestone-02/src/client/componentScripts/headerBar.js
@@ -1,5 +1,13 @@
 export function renderHeaderBar(element) {
-    element.innerHTML = "<img src='./images/RendezvousIcon.png' width='60px' height='60px'></img><h2 style='margin-left: 10px; margin-top: 0; margin-bottom: 0;'>Rendezvous</h2>";
+    element.innerHTML = "\
+        <div id='headerIcon'>\
+            <img src='./images/RendezvousIcon.png' width='60px' height='60px'></img>\
+            <h2 style='margin-left: 10px; margin-top: 0; margin-bottom: 0;'>Rendezvous</h2>\
+        </div>\
+        <div id='settings-button-container'>\
+            <button class='nav-button' id='settingsBtn'>Settings</button>\
+        </div>\
+        ";
 }
 
 
