a1e333c05ce815145767234d36800e8d5dcf1a7a
Wong-Benjamin <benjaminwong02@gmail.com>
2024-05-09 21:31:55 -0400

    Database.js function signatures
    Creates signatures for all the functions to be added to the database, for the server to reference.

--- src/docs/milestone-03/src/server/database.js
+++ src/docs/milestone-03/src/server/database.js
@@ -2,6 +2,76 @@ import PouchDB from "pouchdb";
 
 // Pouch DB docs here: https://pouchdb.com/
 
+// define some classes so that our operations are a bit less confusing
+class UserCollection {
+  static nextId = 0;
+  users = {}
+  async addUser(name, email) {
+      let newUser = new User(name, email);
+      newUser._id = UserCollection.nextId;
+      UserCollection.nextId += 1;
+      this.users[newUser._id] = newUser;
+      return newUser._id;
+  }
+  async getUser(id) {return this.users[id];}
+}
+class User {
+  constructor(name, email) {
+      this.username = name;
+      this.email = email;
+      this.karma = 0;
+      this.subscribedBoards = [];
+      this.eventsCreated = [];
+      this.eventsAttending = [];
+  }
+}
+
+class BoardCollection {
+  static nextId = 0;
+  boards = {}
+  async addBoard(name, type, description) {
+      let newBoard = new Board(name, type, description);
+      newBoard._id = BoardCollection.nextId;
+      BoardCollection.nextId += 1;
+      this.boards[newBoard._id] = newBoard;
+      return newBoard._id;
+  }
+  async getBoard(id) {return this.boards[id];}
+}
+class Board {
+  constructor(name, type, description) {
+      this.name = name;
+      this.type = type;
+      this.description = description;
+      this.subscribedUsers = [];
+      this.events = [];
+  }
+}
+
+class EventCollection {
+  static nextId = 0;
+  events = {}
+  async addEvent(author, title, description, startTime, endTime, location, board) {
+      let newEvent = new Event(author, title, description, startTime, endTime, location, board);
+      newEvent._id = EventCollection.nextId;
+      EventCollection.nextId += 1;
+      this.events[newEvent._id] = newEvent;
+      return newEvent._id;
+  }
+  async getEvent(id) {return this.events[id];}
+}
+class Event {
+  constructor(author, title, description, startTime, endTime, location, board) {
+      this.author = author;
+      this.title = title;
+      this.description = description;
+      this.startTime = startTime;
+      this.endTime = endTime;
+      this.location = location;
+      this.board = board;
+  }
+}
+
 /**
  * Initializes a PouchDB database with specified collections if they do not
  * exist.
@@ -16,6 +86,8 @@ const initdb = async (dbname) => {
   // Initialize the database if it doesn't exist
   const db = new PouchDB(dbname);
 
+
+  // TODO: MAKE ALL OF THESE COLLECTIONS, YOU FUCKING IDIOT
   // Get the events collection If it doesn't exist, create it.
   try {
     const events = await db.get("events");
@@ -65,11 +137,48 @@ const Database = async (dbname) => {
   const getDB = () => new PouchDB(dbname);
 
   const obj = {
+
     /**
-     * Example(?) function?
-     * Grabs all events upon request. no input requred.
+     * getEventByID
+     * 
+     * Returns an event given its ID number
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } ID, the ID number of the requested board.
+     * @return { Promise } That resolves to the event and its data if it is found, or an error informing otherwise.
      */
-    getAllEvents: async () => {
+
+
+    /**
+     * getBoardByID
+     * 
+     * Returns an board given its ID number.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } ID, the ID number of the requested board.
+     * @return { Promise } That resolves to the board and its data if it is found, or an error informing otherwise.
+     */
+
+
+    /**
+     * getUserByID
+     * 
+     * Returns a user given their ID number.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } ID, the ID number of the requested board.
+     * @return { Promise } That resolves to the user and its data if it is found, or an error informing otherwise.
+     */
+    
+    /**
+     * getAllEvents
+     * 
+     * Returns all events upon request.
+     *
+     * @author: Benjamin Wong
+     * @return { Promise } That resolves to a list of all events, or an error informing otherwise.
+     */
+    getAllEvents: async () => {W
       try {
         const db = getDB();
 
@@ -90,6 +199,115 @@ const Database = async (dbname) => {
         };
       }
     },
+
+
+    /**
+     * getAllBoards
+     * 
+     * Returns all boards upon request.
+     *
+     * @author: Benjamin Wong
+     * @return { Promise } That resolves to a list of all boards, or an error informing otherwise.
+     */
+
+
+    /**
+     * getAllEvents
+     * 
+     * Returns all users upon request.
+     *
+     * @author: Benjamin Wong
+     * @return { Promise } That resolves to a list of all users, or an error informing otherwise.
+     */
+
+    /**
+     * createEvent
+     * 
+     * Given the requisite info, creates a new event and adds it to the database
+     *
+     * @author: Benjamin Wong
+     * @param { Event } event, an event class with correct details.
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+    /**
+     * createBoard
+     * 
+     * Given the requisite info, creates a new board and adds it to the database
+     *
+     * @author: Benjamin Wong
+     * @param { Board } board, an event class with correct details.
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+    /**
+     * deleteEvent
+     * 
+     * Given the requisite info, deletes an event with the specified ID from the database.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } eventID, the ID of the event to be deleted.
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+    /**
+     * attendEvent
+     * 
+     * Given the requisite info, adds an event to a user's list of events to attend.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } eventID, the ID of the event to attend
+     * @param { User } userID, the user to attend this event
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+
+    /**
+     * addBoard
+     * 
+     * Given the requisite info, adds an board to a user's list of followed boards.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } boardID , the ID of the event to follow
+     * @param { User } userID, the user to follow this board
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+    /**
+     * unattendEvent
+     * 
+     * Given the requisite info, removes an event to a user's list of events to attend.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } eventID, the ID of the event to stop attending
+     * @param { User } userID, the user to unattend this event
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+
+    /**
+     * unfollowBoard
+     * 
+     * Given the requisite info, removes a board to a user's list of followed boards.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } boardID , the ID of the event to unfollow
+     * @param { User } userID, the user to unfollow this board
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+    /**
+     * deleteBoard
+     * 
+     * Given a board's ID, deletes it from the DB if there is noone in attendance.
+     *
+     * @author: Benjamin Wong
+     * @param { Integer } eventID, the ID of the event to delete
+     * @return { Promise } That resolves an indication of success or error otherwise.
+     */
+
+
+
   };
 
   return obj;
